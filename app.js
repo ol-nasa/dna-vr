0b7d413688e9dbe5bea0302fc9e6c2685f3c7625
ol-nasa <Olganasadik@gmail.com>
2026-07-23 17:12:08 +0300

    Add VRButton for WebXR support in app.js

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 import {World} from "./world.js";
 import {Controls} from "./controls.js";
 
+import {VRButton} from "three/addons/webxr/VRButton.js";
+
+
 const world = new World();
+
+
+document.body.appendChild(
+
+    VRButton.createButton(world.renderer)
+
+);
+
 
 const controls = new Controls(world);
 
+
+world.setControls = function(controls){
+
+    this.controls = controls;
+
+};
+
+
 world.setControls(controls);
 
-world.renderer.setAnimationLoop(() => {
+
+
+world.renderer.setAnimationLoop(()=>{
 
     controls.update();
 
